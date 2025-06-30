92c6321d31b9272264492f87d472349c54c16074
Gocha <gochazautashvili2017@gmail.com>
2025-06-30 13:16:28 +0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.routes'),
+  },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.routes') },
+  { path: '', loadChildren: () => import('./modules/root/root.routes') },
+];
